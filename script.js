e719218d124df4e21907949e31fa8fca70a8f126
ariunzulm <munkhbatt.ariunzul@gmail.com>
2025-12-20 14:58:56 +0800

    Commit

---
+++ script.js
@@ -0,0 +1,9 @@
+const allReviewButton$ = document.getElementById("allReviewsButton");
+const amazonButtonButton$ = document.getElementById("amazonButton");
+const googleButton$ = document.getElementById("googleButton");
+const upworkButton$ = document.getElementById("upworkButton");
+
+const writeButton$ = document.getElementById("writeButton");
+const messageButton$ = document.getElementById("messageButton");
+
+//
