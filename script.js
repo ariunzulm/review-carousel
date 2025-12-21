0b0a405020c9daefa9a7b43d0a5e35015798e2bb
ariunzulm <munkhbatt.ariunzul@gmail.com>
2025-12-21 17:32:59 +0800

    swiper

--- script.js
+++ script.js
@@ -6,4 +6,14 @@ const upworkButton$ = document.getElementById("upworkButton");
 const writeButton$ = document.getElementById("writeButton");
 const messageButton$ = document.getElementById("messageButton");
 
-//
+const swiper = new Swiper(".swiper", {
+  loop: true,
+
+  pagination: {
+    el: ".swiper-pagination",
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+});
